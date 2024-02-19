ac068f1838579a7161bb00488507fb6bad8cf8cf
Josip Stanic <josip.stanic@avaloq.com>
2024-02-19 15:40:02 +0100

    Feature: Support Cloudflare Spectrum for arbitrary TCP/UDP services

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,7 @@ output "service_publishing" {
     tunnels               = cloudflare_tunnel.these
     access_applications   = cloudflare_access_application.these
     access_service_tokens = cloudflare_access_service_token.these
+    spectrum_applications = cloudflare_spectrum_application.these
   }
   sensitive   = true
   description = "Service Publishing output object."
--- variables.tf
+++ variables.tf
@@ -39,7 +39,22 @@ variable "service_publishing" {
       account_id = string,
       name       = string,
     })))
+    spectrum_applications = optional(list(object({
+      zone_id       = string,
+      name          = string,
+      protocol      = string,
+      origin_direct = optional(list(string)),
+      origin_dns = optional(object({
+        name = string,
+      })),
+      origin_port = optional(number)
+      tls         = optional(string),
+    })))
   })
   # Validation:
   ## If tunnel ommited, make sure in Records value is given.
+  ### If Specturm application defined mutually exclusive with record.
+  ### If Spectrum application, one of origin_direct or origin_dns must be provided and are mutually exclusive.
+  ### If Spectrum application origin_dns is not nil, enforce setting of origin_port
+  ### If Spectrum application is TCP, enum for tls attribute see (except for "flexible"): https://developers.cloudflare.com/spectrum/reference/configuration-options/#edge-tls-termination.
 }
