c905488a55675698f94915116f9ae03747008a07
Josip Stanic <114063472+josip-stanic@users.noreply.github.com>
2024-03-27 11:24:55 +0100

    Feature: Dynamically lookup zone_id (#23)

--- record.tf
+++ record.tf
@@ -1,7 +1,7 @@
 # Records
 resource "cloudflare_record" "these" {
   for_each = (var.service_publishing.records != null) ? ({ for record in var.service_publishing.records : record.name => record }) : ({})
-  zone_id  = each.value.zone_id
+  zone_id  = data.cloudflare_zone.these["${each.key}"].id
   name     = each.value.name
   # If Tunnel found with matching name assume CNAME for type of record, else take value for DNS record from input
   type = (try(cloudflare_tunnel.these["${each.key}"], null) != null) ? ("CNAME") : (each.value.type)
